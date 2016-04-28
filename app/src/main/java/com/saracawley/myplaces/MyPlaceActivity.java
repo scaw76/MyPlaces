4906996d1fb4656e0407ea72d4552b57854a1c53
scaw76 <scawley@dmail.dixie.edu>
2016-04-28 10:05:41 -0600

    Got a fix on my loction

--- app/src/main/java/com/saracawley/myplaces/MyPlaceActivity.java
+++ app/src/main/java/com/saracawley/myplaces/MyPlaceActivity.java
@@ -1,8 +1,13 @@
 package com.saracawley.myplaces;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.util.UUID;
 
@@ -11,6 +16,8 @@ import java.util.UUID;
  */
 public class MyPlaceActivity extends SingleFragmentActivity {
     private static final String EXTRA_PLACE_ID = "com.saracawley.android.myplace.place_id";
+
+    private static final int REQUEST_ERROR = 0;
 
     public static Intent newIntent(Context packageContext, UUID placeID){
         Intent intent = new Intent(packageContext, MyPlaceActivity.class);
@@ -25,4 +32,20 @@ public class MyPlaceActivity extends SingleFragmentActivity {
         return MyPlaceFragment.newInstance(placeId);
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if(errorCode != ConnectionResult.SUCCESS){
+            Dialog errorDialog = googleApiAvailability.getErrorDialog(this, errorCode,  REQUEST_ERROR,
+                    new DialogInterface.OnCancelListener(){
+                        @Override
+                        public void onCancel(DialogInterface dialog) {
+                            finish();
+                        }
+                    });
+            errorDialog.show();
+        }
+    }
 }
--- app/src/main/java/com/saracawley/myplaces/MyPlaceFragment.java
+++ app/src/main/java/com/saracawley/myplaces/MyPlaceFragment.java
@@ -1,15 +1,18 @@
 package com.saracawley.myplaces;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Location;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -22,6 +25,12 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,6 +40,7 @@ import java.util.UUID;
  * Created by sara on 4/26/2016.
  */
 public class MyPlaceFragment extends Fragment {
+
     private static final String TAG = "MyPlaceFragment";
     private static final String ARG_PLACE_ID = "place_id";
     private static final int REQUEST_PHOTO = 2;
@@ -40,7 +50,10 @@ public class MyPlaceFragment extends Fragment {
     private ImageButton mImageButton;
     private ImageView mPhotoView;
 
-    public static MyPlaceFragment newInstance(UUID placeID){
+    private GoogleApiClient mClient;
+
+
+    public static MyPlaceFragment newInstance(UUID placeID) {
         Bundle args = new Bundle();
         args.putSerializable(ARG_PLACE_ID, placeID);
 
@@ -53,9 +66,39 @@ public class MyPlaceFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+
+        mClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        getActivity().invalidateOptionsMenu();
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
+
         UUID place_id = (UUID) getArguments().getSerializable(ARG_PLACE_ID);
         mPlace = PlaceManger.get(getActivity()).getPlace(place_id);
         mPhotoFile = PlaceManger.get(getActivity()).getPhotoFile(mPlace);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().invalidateOptionsMenu();
+        mClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mClient.disconnect();
     }
 
     @Override
@@ -68,9 +111,9 @@ public class MyPlaceFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_place,container,false);
+        View v = inflater.inflate(R.layout.fragment_place, container, false);
 
-        mNameTextView = (TextView)v.findViewById(R.id.name);
+        mNameTextView = (TextView) v.findViewById(R.id.name);
         mImageButton = (ImageButton) v.findViewById(R.id.camera);
 
 
@@ -78,12 +121,12 @@ public class MyPlaceFragment extends Fragment {
         final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         boolean canTakePhoto = mPhotoFile != null && captureImage.resolveActivity(packageManager) != null;
         mImageButton.setEnabled(canTakePhoto);
-        if(canTakePhoto){
+        if (canTakePhoto) {
             Log.i("MyPlaceFragment", " can take photo");
             Uri uri = Uri.fromFile(mPhotoFile);
             captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
         }
-        mImageButton.setOnClickListener(new View.OnClickListener(){
+        mImageButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
@@ -115,14 +158,15 @@ public class MyPlaceFragment extends Fragment {
         });
         return v;
     }
-    String ReadExif(String file) {
-        String exif = "exif: "+ file;
 
-        try{
+    String ReadExif(String file) {
+        String exif = "exif: " + file;
+
+        try {
             ExifInterface exifInterface = new ExifInterface(file);
-            exif += "Image La t"+ exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
-            exif += "Image Lon "+ exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
-        }catch (IOException e){
+            exif += "Image La t" + exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+            exif += "Image Lon " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
+        } catch (IOException e) {
             Log.e("MyPlaceFragment", "ReadExif " + e);
         }
         return exif;
@@ -130,12 +174,13 @@ public class MyPlaceFragment extends Fragment {
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode != Activity.RESULT_OK){
+        if (resultCode != Activity.RESULT_OK) {
             return;
-        }if(requestCode == REQUEST_PHOTO){
-            try{
+        }
+        if (requestCode == REQUEST_PHOTO) {
+            try {
                 ExifInterface exifInterface = new ExifInterface(mPhotoFile.getPath());
-                String lat =  exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
+                String lat = exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE);
                 String lon = exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
             } catch (IOException e) {
                 e.printStackTrace();
@@ -151,19 +196,60 @@ public class MyPlaceFragment extends Fragment {
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.fragment_place, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mClient.isConnected());
+
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_item_delete_place:
                 PlaceManger.get(getActivity()).deletePlace(mPlace);
                 getActivity().finish();
+                return true;
+            case R.id.action_locate:
+                findMe();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    private void findMe() {
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },0);
+            return;
+        }
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Toast.makeText(getActivity(),"Got a fix:" + location, Toast.LENGTH_SHORT).show();
+                        Log.i(TAG, "Got a fix:" + location);
+                    }
+                });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                findMe();
+            } else {
+                Toast.makeText(getActivity(), "Location permission denied.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     private void updatePhotoView(){
         if(mPhotoFile == null || !mPhotoFile.exists()){
             mPhotoView.setImageDrawable(null);
